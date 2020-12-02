5b72ee166d6fd496dca4874c00cb5b13f59e34d9
Pat Quest <pn.quest@gmail.com>
2020-12-02 05:47:04 -0500

    Day 2

---
+++ Aoc2020/Day2/PasswordData.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+
+namespace Day2
+{
+    public class PasswordData
+    {
+        public int MinOccurances { get; set; }
+        public int MaxOccurances { get; set; }
+        public char TargetChar { get; set; }
+        public string Password { get; set; }
+
+        public bool ValidatePasswordPart1()
+        {
+            System.Collections.Generic.Dictionary<char, int> chars = Password.ToCharArray()
+                .GroupBy(c => c)
+                .ToDictionary(c => c.Key, c => c.Count());
+
+            if(chars.TryGetValue(TargetChar, out int count))
+            {
+                return count >= MinOccurances && count <= MaxOccurances;
+            }
+
+            return false;
+        }
+
+        public bool ValidatePasswordPart2()
+        {
+            if(MinOccurances > Password.Length || MaxOccurances > Password.Length)
+            {
+                return false;
+            }
+
+            return Password[MinOccurances - 1] == TargetChar ^ Password[MaxOccurances - 1] == TargetChar;
+        }
+    }
+}
---
+++ Aoc2020/Day2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Pidgin;
+using static Pidgin.Parser;
+using static Pidgin.Parser<char>;
+
+namespace Day2
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string[] lines = await File.ReadAllLinesAsync("./input.txt");
+
+            Parser<char, PasswordData> parser = Num.Then(Char('-'), (f, _) => new PasswordData { MinOccurances = f })
+                                 .Then(Num, (d, n) => { d.MaxOccurances = n; return d; })
+                                 .Then(Whitespace, (d, _) => d)
+                                 .Then(Any, (d, c) => { d.TargetChar = c; return d; })
+                                 .Then(Char(':'), (d, _) => d)
+                                 .Then(Whitespace, (d, _) => d)
+                                 .Then(Any.ManyString(), (d, p) => { d.Password = p; return d; });
+
+            PasswordData[] parsed = lines
+                            .Select(l => parser.ParseOrThrow(l))
+                            .ToArray();
+
+            int validCount1 = parsed
+                .Count(c => c.ValidatePasswordPart1());
+
+            Console.WriteLine($"Part 1: {validCount1}");
+
+            int validCount2 = parsed.Count(c => c.ValidatePasswordPart2());
+
+            Console.WriteLine($"Part 2: {validCount2}");
+        }
+    }
+}
