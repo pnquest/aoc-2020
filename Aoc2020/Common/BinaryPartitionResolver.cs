6405ad6121fe2ba6ee04f3bff506b2d18b79c3a6
Pat Quest <pn.quest@gmail.com>
2020-12-05 09:25:51 -0500

    day 5

---
+++ Aoc2020/Common/BinaryPartitionResolver.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Common
+{
+    public class BinaryPartitionResolver
+    {
+        private int _min;
+        private int _max;
+        public bool IsResolved => _min == _max;
+        public int? Result => IsResolved ? _min : null;
+
+        public BinaryPartitionResolver(int min, int max)
+        {
+            _min = min;
+            _max = max;
+        }
+
+        public enum PartitionHalf
+        {
+            Upper,
+            Lower,
+        }
+
+        public bool Partition(PartitionHalf half)
+        {
+            if(IsResolved)
+            {
+                throw new InvalidOperationException("Cannot partition when already solved");
+            }
+
+            int move = (_max - _min + 1) / 2;
+            switch(half)
+            {
+                case PartitionHalf.Upper:
+                    _min += move;
+                    break;
+
+                case PartitionHalf.Lower:
+                    _max -= move;
+                    break;
+            }
+
+            return IsResolved;
+        }
+    }
+}
---
+++ Aoc2020/Day5/Program.cs
@@ -0,0 +1,85 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] passes = File.ReadAllLines("./input.txt");
+            Part1(passes);
+            Part2(passes);
+        }
+
+        private static void Part1(string[] passes)
+        {
+            int maxId = 0;
+            foreach (string pass in passes)
+            {
+                int id = ComputeSeatId(pass);
+
+                if (id > maxId)
+                {
+                    maxId = id;
+                }
+            }
+
+            Console.WriteLine($"Part 1: Max Id: {maxId}");
+        }
+
+        private static void Part2(string[] passes)
+        {
+            List<int> ids = passes.Select(ComputeSeatId).ToList();
+            ids.Sort();
+
+            for(int i = 0; i < ids.Count - 1; i++)
+            {
+                if(ids[i] != ids[i + 1] - 1)
+                {
+                    Console.WriteLine($"Part 2: My Seat ID is {ids[i] + 1}");
+                    return;
+                }
+            }
+        }
+
+        private static int ComputeSeatId(string pass)
+        {
+            BinaryPartitionResolver rowResolver = new(0, 127);
+            foreach (char c in pass[0..7])
+            {
+                BinaryPartitionResolver.PartitionHalf half = c switch
+                {
+                    'F' => BinaryPartitionResolver.PartitionHalf.Lower,
+                    'B' => BinaryPartitionResolver.PartitionHalf.Upper,
+                    _ => throw new ArgumentException($"{c} is not valid")
+                };
+
+                rowResolver.Partition(half);
+            }
+
+            int row = rowResolver.Result.Value;
+
+            BinaryPartitionResolver colResolver = new(0, 7);
+            foreach (char c in pass[7..])
+            {
+                BinaryPartitionResolver.PartitionHalf half = c switch
+                {
+                    'L' => BinaryPartitionResolver.PartitionHalf.Lower,
+                    'R' => BinaryPartitionResolver.PartitionHalf.Upper,
+                    _ => throw new ArgumentException($"{c} is not valid")
+                };
+
+                colResolver.Partition(half);
+            }
+
+            int col = colResolver.Result.Value;
+
+            int id = row * 8 + col;
+            return id;
+        }
+    }
+}
