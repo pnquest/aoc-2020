2fc8f645bee8ae7ac2805773322ad485fd337101
Pat Quest <pn.quest@gmail.com>
2020-12-08 07:44:19 -0500

    Day 8

---
+++ Aoc2020/Day8/IInstruction.cs
@@ -0,0 +1,67 @@
+﻿namespace Day8
+{
+    public interface IInstruction
+    {
+        void Execute(ref int inst, ref int acc);
+        IInstruction FlipInstruction();
+    }
+
+    public class AccInstruction : IInstruction
+    {
+        private readonly int _amount;
+        public AccInstruction(int amount)
+        {
+            _amount = amount;
+        }
+
+        public void Execute(ref int inst, ref int acc)
+        {
+            acc += _amount;
+            inst++;
+        }
+
+        public IInstruction FlipInstruction()
+        {
+            return this;
+        }
+    }
+
+    public class JmpInstruction : IInstruction
+    {
+        private readonly int _distance;
+
+        public JmpInstruction(int distance)
+        {
+            _distance = distance;
+        }
+
+        public void Execute(ref int inst, ref int acc)
+        {
+            inst += _distance;
+        }
+
+        public IInstruction FlipInstruction()
+        {
+            return new NopInstruction(_distance);
+        }
+    }
+
+    public class NopInstruction : IInstruction
+    {
+        private readonly int _value;
+        public NopInstruction(int value)
+        {
+            _value = value;
+        }
+
+        public void Execute(ref int inst, ref int acc)
+        {
+            inst++;
+        }
+
+        public IInstruction FlipInstruction()
+        {
+            return new JmpInstruction(_value);
+        }
+    }
+}
---
+++ Aoc2020/Day8/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IInstruction[] instructions = File
+                .ReadAllLines("./input.txt")
+                .Select(ParseLine)
+                .ToArray();
+
+            Part1(instructions);
+            Part2(instructions);
+        }
+
+        private static void Part2(IInstruction[] instructions)
+        {
+            int instr = 0;
+            int acc = 0;
+            HashSet<int> visited = new();
+            int flippedIndex = -1;
+            while (instr < instructions.Length)
+            {
+                instr = 0;
+                acc = 0;
+                visited.Clear();
+
+                //flip back
+                if (flippedIndex >= 0)
+                {
+                    FlipInstruction(instructions, ref flippedIndex);
+                }
+
+                flippedIndex++;
+
+                FlipInstruction(instructions, ref flippedIndex);
+
+                while (instr < instructions.Length && visited.Add(instr))
+                {
+                    IInstruction cur = instructions[instr];
+                    cur.Execute(ref instr, ref acc);
+                }
+            }
+
+            Console.WriteLine($"Part 2: Acc = {acc}");
+        }
+
+        private static void FlipInstruction(IInstruction[] stack, ref int index)
+        {
+            while(stack[index] is AccInstruction)
+            {
+                index++;
+            }
+
+            stack[index] = stack[index].FlipInstruction();
+        }
+
+        private static void Part1(IInstruction[] instructions)
+        {
+            int instr = 0;
+            int acc = 0;
+            HashSet<int> visited = new();
+
+            while (visited.Add(instr))
+            {
+                IInstruction cur = instructions[instr];
+                cur.Execute(ref instr, ref acc);
+            }
+
+            Console.WriteLine($"Part 1: Acc = {acc}");
+        }
+
+        private static IInstruction CreateInstruction(string instr, int amount)
+        {
+            return instr switch
+            {
+                "acc" => new AccInstruction(amount),
+                "jmp" => new JmpInstruction(amount),
+                "nop" => new NopInstruction(amount),
+                _ => throw new ArgumentException(nameof(instr))
+            };
+        }
+
+        private static IInstruction ParseLine(string line)
+        {
+            string[] split = line.Split(' ');
+            return CreateInstruction(split[0], int.Parse(split[1]));
+        }
+    }
+}
