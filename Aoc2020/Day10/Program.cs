e20af0e00abd71cd19022d860b6d462d16dab12c
Pat Quest <pn.quest@gmail.com>
2020-12-11 10:23:46 -0500

    Days 10 and 11

--- Aoc2020/Day10/Program.cs
+++ Aoc2020/Day10/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Day10
 {
@@ -6,7 +9,122 @@ namespace Day10
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> items = File
+                .ReadAllLines("./input.txt")
+                .Select(l => int.Parse(l))
+                .ToList();
+
+            items.Insert(0, 0);
+
+            items.Sort();
+            items.Add(items.Last() + 3);
+            Part1(items);
+            Part2(items);
+        }
+
+        private static void Part2(List<int> items)
+        {
+            List<int> distances = new();
+            ComputeDistances(items, distances);
+
+            int[] dist = distances.ToArray();
+            long score = 1;
+            for (int i = 0; i < dist.Length; i++)
+            {
+                if (i + dist[i] < dist.Length)
+                {
+                    int first1Index = FirstIndexOf1(dist, i, dist[i]);
+                    if ((first1Index - i) > 0)
+                    {
+                        var slice = dist[i..(i + (first1Index - i) + 1)];
+                        if (MatchFlipGroup(slice))
+                        {
+                            score *= slice.Length switch
+                            {
+                                4 => 7,
+                                3 => 4,
+                                2 => 2,
+                                _ => throw new Exception("ugh")
+                            };
+                            i += slice.Length - 1;
+                        }
+                    }
+
+                }
+            }
+
+            Console.WriteLine($"Part 2: {score}");
+        }
+
+        private static void ComputeDistances(List<int> items, List<int> distances)
+        {
+            for (int i = 0; i < items.Count - 1; i++)
+            {
+                if (i + 3 < items.Count && items[i + 3] - items[i] == 3)
+                {
+                    distances.Add(3);
+                }
+                else if (i + 2 < items.Count && items[i + 2] - items[i] == 2)
+                {
+                    distances.Add(2);
+                }
+                else
+                {
+                    distances.Add(1);
+                }
+            }
+        }
+
+        private static int FirstIndexOf1(int[] array, int startIndex, int searchDistance)
+        {
+            for(int i = startIndex; i <= startIndex + searchDistance; i++)
+            {
+                if(array[i] == 1)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        private static bool MatchFlipGroup(int[] slice)
+        {
+            if (slice[^1] == 1)
+            {
+                for (int j = 1; j < slice.Length; j++)
+                {
+                    if (j + slice[j] < slice.Length)
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        private static void Part1(List<int> items)
+        {
+            int oneOffCount = 0;
+            int threeOffCount = 0;
+            for (int i = 1; i < items.Count; i++)
+            {
+                int diff = items[i] - items[i - 1];
+
+                if (diff == 1)
+                {
+                    oneOffCount++;
+                }
+                else if (diff == 3)
+                {
+                    threeOffCount++;
+                }
+            }
+
+            Console.WriteLine($"Part 1: {oneOffCount} * {threeOffCount} = {oneOffCount * threeOffCount}");
         }
     }
 }
---
+++ Aoc2020/Day11/Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Part1();
+            Part2();
+        }
+
+        private static void Part2()
+        {
+            char[][] rows;
+            int maxHeight, maxWidth;
+            GetPuzzleData(out rows, out maxHeight, out maxWidth);
+
+            bool isChanged;
+            int occupiedCount;
+            do
+            {
+                char[][] newRows = ComputeNextStage(rows, maxHeight, maxWidth, GetRayTracingOccupiedCount, 5, out isChanged, out occupiedCount);
+
+                rows = newRows;
+
+            } while (isChanged);
+
+            Console.WriteLine($"Part 2: {occupiedCount}");
+        }
+
+        private static void Part1()
+        {
+            char[][] rows;
+            int maxHeight, maxWidth;
+            GetPuzzleData(out rows, out maxHeight, out maxWidth);
+
+            bool isChanged;
+            int occupiedCount;
+            do
+            {
+                char[][] newRows = ComputeNextStage(rows, maxHeight, maxWidth, GetAdjacentOccupiedCount, 4, out isChanged, out occupiedCount);
+
+                rows = newRows;
+
+            } while (isChanged);
+
+            Console.WriteLine($"Part 1: {occupiedCount}");
+        }
+
+        private static void GetPuzzleData(out char[][] rows, out int maxHeight, out int maxWidth)
+        {
+            rows = File.ReadAllLines("./input.txt").Select(l => l.ToCharArray()).ToArray();
+            maxHeight = rows.Length;
+            maxWidth = rows[0].Length;
+        }
+
+        private static char[][] ComputeNextStage(
+            char[][] rows,
+            int maxHeight,
+            int maxWidth,
+            Func<char[][], int, int, int, int, int> adjacentSeatCountDelegate,
+            int adjacentSeatTolerance,
+            out bool isChanged,
+            out int occupiedCount)
+        {
+            isChanged = false;
+            occupiedCount = 0;
+
+            char[][] newRows = new char[maxHeight][];
+            for (int y = 0; y < maxHeight; y++)
+            {
+                newRows[y] = new char[maxWidth];
+                for (int x = 0; x < maxWidth; x++)
+                {
+                    char newChar = rows[y][x] switch
+                    {
+                        'L' when adjacentSeatCountDelegate(rows, x, y, maxWidth, maxHeight) == 0 => '#',
+                        '#' when adjacentSeatCountDelegate(rows, x, y, maxWidth, maxHeight) >= adjacentSeatTolerance => 'L',
+                        _ => rows[y][x]
+                    };
+
+                    if (rows[y][x] != newChar)
+                    {
+                        isChanged = true;
+                    }
+                    if (newChar == '#')
+                    {
+                        occupiedCount++;
+                    }
+                    newRows[y][x] = newChar;
+                }
+            }
+
+            return newRows;
+        }
+
+        private static int GetRayTracingOccupiedCount(char[][] rows, int x, int y, int maxX, int maxY)
+        {
+            Span<int> steps = stackalloc[] { -1, 0, 1 };
+            int res = 0;
+            foreach(int yOffset in steps)
+            {
+                foreach(int xOffset in steps)
+                {
+                    if(yOffset != 0 || xOffset != 0)
+                    {
+                        int lookX = x;
+                        int lookY = y;
+                        do
+                        {
+                            lookX += xOffset;
+                            lookY += yOffset;
+                        } while (IsInBounds(lookX, lookY, maxX, maxY) && rows[lookY][lookX] == '.');
+
+                        if(IsInBounds(lookX, lookY, maxX, maxY) && rows[lookY][lookX] == '#')
+                        {
+                            res++;
+                        }
+                    }
+                }
+            }
+
+            return res;
+        }
+
+        private static bool IsInBounds(int x, int y, int maxX, int maxY)
+        {
+            return x >= 0
+                && x < maxX
+                && y >= 0
+                && y < maxY;
+        }
+
+        private static int GetAdjacentOccupiedCount(char[][] rows, int x, int y, int maxX, int maxY)
+        {
+            Span<int> steps = stackalloc[] { -1, 0, 1 };
+            int res = 0;
+            foreach(int yOffset in steps)
+            {
+                foreach(int xOffset in steps)
+                {
+                    int lookX = x + xOffset;
+                    int lookY = y + yOffset;
+                    if((yOffset != 0 || xOffset != 0)
+                        && IsInBounds(lookX, lookY, maxX, maxY)
+                        && rows[lookY][lookX] == '#')
+                    {
+                        res++;
+                    }
+                }
+            }
+
+            return res;
+        }
+    }
+}
